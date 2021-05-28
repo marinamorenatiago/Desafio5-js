f1dba7489d291fd48136c5377efa5ac92aa305b7
marinamorenatiago <morenatiago@gmail.com>
2021-05-27 21:47:56 -0300

    subindo o desafio 5 js

---
+++ main.js
@@ -0,0 +1,25 @@
+let itens = ["Leite", "Queijo", "Manteiga", "Iogurte", "Requeijão"]
+
+console.log(itens)
+
+itens.unshift("Ovo")
+
+console.log(itens)
+
+itens.length = 5
+
+console.log(itens)
+
+itens.push("Frango", "Carne")
+
+console.log(itens)
+
+itens.shift()
+
+console.log(itens)
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+
+console.log(numbers)
